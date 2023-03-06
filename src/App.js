76a24d076ab80bd99d50c23316642bfe1fb98fbd
Novasrikanth <srikanth.kuchipudi4@gmail.com>
2023-03-06 14:33:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_m0hA6F8Oxz43Ibv4LAafyLh57JYsa-hgsg&usqp=CAU"  className="App-logo" alt="logo" />
+        <p style={{color:"red"}}>
+          welcome to church of christ Hyderabad
+        </p>
+        <a
+          className="App-link"
+          href="http://www.churchofchristhyd.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Church of christ official web page
+        </a>
+        <a href="https://www.youtube.com/@churchofchristhyd/channels" >Youtube</a>
+        <a href="https://www.facebook.com/groups/churchofchristhyd" >Facebook</a>
+        <a href= "mailto : kurrushdrk12gmail.com" target="_blank">Mail to</a>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
